f2fa8e554f07f2e78cb666c8165704244f2bd2c8
Nithishkumar S <nithish687894@gmail.com>
2026-04-28 18:58:49 +0530

    UC8: Continuous turn-based game loop implemented

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,39 +1,108 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class TicTacToe {
 
-    // Board setup
     static char[] board = {'1','2','3','4','5','6','7','8','9'};
+    static Scanner sc = new Scanner(System.in);
     static Random random = new Random();
 
     public static void main(String[] args) {
-        System.out.println("Before Computer Move:");
-        printBoard();
 
-        computerMove(); // UC7
+        boolean gameOver = false;
+        char currentPlayer = 'X'; // X = User, O = Computer
 
-        System.out.println("\nAfter Computer Move:");
-        printBoard();
+        while (!gameOver) {
+
+            printBoard();
+
+            if (currentPlayer == 'X') {
+                playerMove();
+            } else {
+                computerMove();
+            }
+
+            // Check win
+            if (checkWin()) {
+                printBoard();
+                System.out.println("Player " + currentPlayer + " wins!");
+                gameOver = true;
+            }
+            // Check draw
+            else if (checkDraw()) {
+                printBoard();
+                System.out.println("Game is a Draw!");
+                gameOver = true;
+            }
+            // Switch turn
+            else {
+                currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
+            }
+        }
     }
 
-    // UC7: Computer makes random valid move
-    public static void computerMove() {
+    // Player move
+    public static void playerMove() {
         int move;
-
         while (true) {
-            move = random.nextInt(9) + 1; // generate 1–9
+            System.out.print("Enter position (1-9): ");
+            move = sc.nextInt();
 
             if (isValidMove(move)) {
-                board[move - 1] = 'O'; // place computer symbol
-                System.out.println("Computer chose position: " + move);
+                board[move - 1] = 'X';
+                break;
+            } else {
+                System.out.println("Invalid move, try again!");
+            }
+        }
+    }
+
+    // Computer move (UC7 reused)
+    public static void computerMove() {
+        int move;
+        while (true) {
+            move = random.nextInt(9) + 1;
+
+            if (isValidMove(move)) {
+                board[move - 1] = 'O';
+                System.out.println("Computer chose: " + move);
                 break;
             }
         }
     }
 
-    // Check if slot is free
+    // Validate move
     public static boolean isValidMove(int move) {
-        return board[move - 1] != 'X' && board[move - 1] != 'O';
+        return move >= 1 && move <= 9 &&
+               board[move - 1] != 'X' &&
+               board[move - 1] != 'O';
+    }
+
+    // Check win
+    public static boolean checkWin() {
+        int[][] winPatterns = {
+            {0,1,2},{3,4,5},{6,7,8},
+            {0,3,6},{1,4,7},{2,5,8},
+            {0,4,8},{2,4,6}
+        };
+
+        for (int[] pattern : winPatterns) {
+            if (board[pattern[0]] == board[pattern[1]] &&
+                board[pattern[1]] == board[pattern[2]]) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // Check draw
+    public static boolean checkDraw() {
+        for (char c : board) {
+            if (c != 'X' && c != 'O') {
+                return false;
+            }
+        }
+        return true;
     }
 
     // Print board
