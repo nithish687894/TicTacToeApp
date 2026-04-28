73617dc0e6b7dc05270e1b364ef5a6b852869461
Nithishkumar S <nithish687894@gmail.com>
2026-04-28 18:59:40 +0530

    UC9: Winning condition implemented using pattern matching

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,117 +1,35 @@
-import java.util.Random;
-import java.util.Scanner;
+public static boolean checkWin(char player) {
 
-public class TicTacToe {
-
-    static char[] board = {'1','2','3','4','5','6','7','8','9'};
-    static Scanner sc = new Scanner(System.in);
-    static Random random = new Random();
-
-    public static void main(String[] args) {
-
-        boolean gameOver = false;
-        char currentPlayer = 'X'; // X = User, O = Computer
-
-        while (!gameOver) {
-
-            printBoard();
-
-            if (currentPlayer == 'X') {
-                playerMove();
-            } else {
-                computerMove();
-            }
-
-            // Check win
-            if (checkWin()) {
-                printBoard();
-                System.out.println("Player " + currentPlayer + " wins!");
-                gameOver = true;
-            }
-            // Check draw
-            else if (checkDraw()) {
-                printBoard();
-                System.out.println("Game is a Draw!");
-                gameOver = true;
-            }
-            // Switch turn
-            else {
-                currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
-            }
+    // Row check
+    for (int i = 0; i < 9; i += 3) {
+        if (board[i] == player &&
+            board[i + 1] == player &&
+            board[i + 2] == player) {
+            return true;
         }
     }
 
-    // Player move
-    public static void playerMove() {
-        int move;
-        while (true) {
-            System.out.print("Enter position (1-9): ");
-            move = sc.nextInt();
-
-            if (isValidMove(move)) {
-                board[move - 1] = 'X';
-                break;
-            } else {
-                System.out.println("Invalid move, try again!");
-            }
+    // Column check
+    for (int i = 0; i < 3; i++) {
+        if (board[i] == player &&
+            board[i + 3] == player &&
+            board[i + 6] == player) {
+            return true;
         }
     }
 
-    // Computer move (UC7 reused)
-    public static void computerMove() {
-        int move;
-        while (true) {
-            move = random.nextInt(9) + 1;
-
-            if (isValidMove(move)) {
-                board[move - 1] = 'O';
-                System.out.println("Computer chose: " + move);
-                break;
-            }
-        }
-    }
-
-    // Validate move
-    public static boolean isValidMove(int move) {
-        return move >= 1 && move <= 9 &&
-               board[move - 1] != 'X' &&
-               board[move - 1] != 'O';
-    }
-
-    // Check win
-    public static boolean checkWin() {
-        int[][] winPatterns = {
-            {0,1,2},{3,4,5},{6,7,8},
-            {0,3,6},{1,4,7},{2,5,8},
-            {0,4,8},{2,4,6}
-        };
-
-        for (int[] pattern : winPatterns) {
-            if (board[pattern[0]] == board[pattern[1]] &&
-                board[pattern[1]] == board[pattern[2]]) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    // Check draw
-    public static boolean checkDraw() {
-        for (char c : board) {
-            if (c != 'X' && c != 'O') {
-                return false;
-            }
-        }
+    // Diagonal check
+    if (board[0] == player &&
+        board[4] == player &&
+        board[8] == player) {
         return true;
     }
 
-    // Print board
-    public static void printBoard() {
-        System.out.println();
-        System.out.println(board[0] + " | " + board[1] + " | " + board[2]);
-        System.out.println("--+---+--");
-        System.out.println(board[3] + " | " + board[4] + " | " + board[5]);
-        System.out.println("--+---+--");
-        System.out.println(board[6] + " | " + board[7] + " | " + board[8]);
+    if (board[2] == player &&
+        board[4] == player &&
+        board[6] == player) {
+        return true;
     }
+
+    return false;
 }
